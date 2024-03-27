88dd83dbf12c502a5c32a92213986454dd6ad058
Stefan Szepe <szepe.stefan@gmail.com>
2024-03-27 18:01:36 +0100

    change pprint to logging

--- solr_example.py
+++ solr_example.py
@@ -1,25 +1,32 @@
-import pprint
+import sys
+import logging
 
 from requests.auth import HTTPBasicAuth
 
 from dspace_rest_client.client import DSpaceClient
 
-url = 'http://localhost:8080/server/api'
-username = 'username@domain.com'
-password = 'password'
+url = "http://localhost:8080/server/api"
+username = "username@domain.com"
+password = "password"
 
 # To auth solr do like this and pass it as the argument in DSpaceClient
-solr_auth = HTTPBasicAuth('user', 'pass')
+solr_auth = HTTPBasicAuth("user", "pass")
 
 # Instantiate DSpace client
-d = DSpaceClient(api_endpoint=url, username=username, password=password,
-                 solr_endpoint='http://localhost:8983/solr/search', solr_auth=None)
+d = DSpaceClient(
+    api_endpoint=url,
+    username=username,
+    password=password,
+    solr_endpoint="http://localhost:8983/solr/search",
+    solr_auth=None,
+)
 
 # Here's an example of a wildcard query with some filters to apply and some fields to return
-results = d.solr_query('*:*',
-                           filters=['search.resourcetype:Item', 'search.entitytype:*'],
-                           fields=['search.resourceid', 'search.entitytype'])
+results = d.solr_query(
+    "*:*",
+    filters=["search.resourcetype:Item", "search.entitytype:*"],
+    fields=["search.resourceid", "search.entitytype"],
+)
 
 for doc in results.docs:
-    pprint.pprint(doc)
-
+    logging.info.plogging.info(doc)
